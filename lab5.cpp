f5120382b2f8d6579b497e42fbbecdb96951071b
선민 <seonmin@seonmins-MacBook-Pro.local>
2020-02-16 00:13:13 -0500

    This is Cmath calculator

--- lab5.cpp
+++ lab5.cpp
@@ -5,61 +5,62 @@
 
 void mathCalculator()
 {
-    
+
     string command;
     double argument1, argument2, answer;
     
-    cin >> command;//prime
+    cin >> command >> argument1;
+
+    cout << "Command    Argument1   [Argument2]     Answer"<< endl;
     
-    while( !cin.fail() )//test
+    while(!cin.fail())//test
     {
         //process
-        cin >> argument1;
-        
+
         if(command == "exponential")
         {
             answer = exp(argument1);
-            cout << "Command    Argument1   [Argument2]     Answer"<< endl;
-            cout << command << "\t" << argument1 << "\t\t\t" <<  answer << endl;
+            cout << command << " \t" << argument1 << " \t\t\t" <<  answer << endl;
         }
         else if(command == "natural-log")
         {
             answer = log(argument1);
-            cout << command << "\t" << argument1 << "\t\t\t" <<  answer << endl;
+            cout << command << " \t" << argument1 << " \t\t\t" <<  answer << endl;
         }
         else if(command == "log")
         {
             answer = log10(argument1);
-            cout << command << "\t" << argument1 << "\t\t\t" <<  answer << endl;
+            cout << command << " \t" << argument1 << " \t\t\t" <<  answer << endl;
         }
         else if(command == "raised-to-the-power")
         {
-            cin >> argument2;
+            cin >>  argument2;
             answer = pow(argument1, argument2);
-            cout << command << "\t" << argument1 << "\t\t\t" << argument2 <<
+            cout << command << " \t" << argument1 << " \t\t\t" << argument2 <<
             "\t\t\t"<<  answer << endl;
         }
         else if(command == "square-root")
         {
             answer = sqrt(argument1);
-            cout << command << "\t" << argument1 << "\t\t\t" <<  answer << endl;
+            cout << command << " \t" << argument1 << " \t\t\t" <<  answer << endl;
         }
         else if(command ==  "ceiling")
         {
             answer = ceil(argument1);
-            cout << command << "\t" << argument1 << "\t\t\t" <<  answer << endl;
+            cout << command << " \t" << argument1 << " \t\t\t" <<  answer << endl;
         }
         else if(command ==  "floor")
         {
             answer = floor(argument1);
-            cout << command << "\t" << argument1 << "\t\t\t" <<  answer << endl;
+            cout << command << " \t" << argument1 << " \t\t\t" <<  answer << endl;
         }
         else
         {
-            cout << "Unkown command: othercommand" << endl;
+            cout << "Unknown command:" << " " << command << endl;
         }
         
         cin >> command;//re-prime
+        cin >> argument1;
     }
     
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
-//
-//  main.cpp
-//  lab05
-//
-//  Created by 선민 on 2/14/20.
-//  Copyright © 2020 선민. All rights reserved.
-//
+
 
 #include "lab5.h"
 
